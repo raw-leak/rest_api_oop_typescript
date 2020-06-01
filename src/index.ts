4535a0599206b3a9708505930bbe42dcd6c1f4b9
mykhaylo.gusak@between.tech <mykhaylo.gusak@between.tech>
2020-06-01 19:56:13 +0200

    Improve project's structuration
    Addapt best practices in project structuration.

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,30 @@
-import cluster from "cluster";
-import os from "os";
+import cluster from 'cluster';
+import os from 'os';
 
 // db config
 import './config/database';
 
 // server config
-import app from './config/app';
+import app from './api/server';
 
 (async () => {
+  if (false) {
+    const cpuCount = os.cpus().length;
 
-    console.log(`app running on port ${app.get('port')}`)
+    console.log(`master with id: ${process.pid} is listening`);
+    console.log(`"${Object.keys(cluster.workers).length}" workers available`);
 
-    if (cluster.isMaster) {
-        const cpuCount = os.cpus().length;
-
-        console.log(`master with id: ${process.pid} is listening`)
-        console.log(`"${Object.keys(cluster.workers).length}" workers available`)
-
-        for (let j = 0; j < cpuCount; j++) {
-            cluster.fork();
-        }
-
-        cluster.on('exit', (worker) => {
-            console.log(`Worker ${worker.id} died'`);
-            console.log(`Staring a new one...`);
-            cluster.fork();
-        });
-
-    } else {
-        await app.listen(app.get('port'));
+    for (let j = 0; j < cpuCount; j++) {
+      cluster.fork();
     }
-})();
 
+    cluster.on('exit', (worker) => {
+      console.log(`Worker ${worker.id} died'`);
+      console.log(`Staring a new one...`);
+      cluster.fork();
+    });
+  } else {
+    await app.listen(app.get('port'));
+    console.log(`Server running on port ${app.get('port')}`);
+  }
+})();
