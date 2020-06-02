eb82e9b81d623a61529ea3cb0e864258a6fdefa7
mykhaylo.gusak@between.tech <mykhaylo.gusak@between.tech>
2020-06-02 18:50:56 +0200

    improve db performance in slow queries

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,28 +1,32 @@
-import mongoose from "mongoose";
-import logger from "morgan";
+import mongoose from 'mongoose';
+import logger from 'morgan';
 
 class Connection {
+  constructor() {
+    const url =
+      process.env.MONGODB_URI || `mongodb://localhost:27017/node-starter`;
 
-    constructor() {
-        const url = process.env.MONGODB_URI || `mongodb://localhost:27017/node-starter`;
+    mongoose.Promise = global.Promise;
+    mongoose.set('useNewUrlParser', true);
+    mongoose.set('useFindAndModify', false);
+    mongoose.set('useCreateIndex', true);
+    mongoose.set('useUnifiedTopology', true);
 
-        mongoose.Promise = global.Promise;
-        mongoose.set("useNewUrlParser", true);
-        mongoose.set("useFindAndModify", false);
-        mongoose.set("useCreateIndex", true);
-        mongoose.set("useUnifiedTopology", true);
+    mongoose.connect(url, {
+      useNewUrlParser: true,
+      poolSize: 10, // Can now run 10 operations at a time
+    });
 
-        mongoose.connect(url);
+    mongoose.connection.on('connected', () => {
+      console.log(
+        logger(`Mongoose connection has been opened successfully to ${url}`)
+      );
+    });
 
-        mongoose.connection.on('connected', () => {
-            console.log(logger(`Mongoose connection has been opened successfully to ${url}`));
-        });
-
-        mongoose.connection.on('error', (error) => {
-            console.log(logger(`Mongoose connection. Error code: 500`), error);
-        });
-    }
-
+    mongoose.connection.on('error', (error) => {
+      console.log(logger(`Mongoose connection. Error code: 500`), error);
+    });
+  }
 }
 
 export default new Connection();
